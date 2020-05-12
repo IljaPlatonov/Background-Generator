f5b75ac53faf63aab85b5af04ce3bbcf3cdd3f49
IljaPlatonov <ilja.mikkeli@gmail.com>
2020-05-12 09:48:06 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+var button = document.getElementById("button");
+
+var setBg = () => {
+	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+	color1.value = "#" + randomColor;
+	color1.innerHTML = "#" + randomColor;
+	}
+
+var setBg1 = () => {
+	var randomColor = Math.floor(Math.random()*16777215).toString(16);
+	color2.value = "#" + randomColor;
+	color2.innerHTML = "#" + randomColor;
+	}
+
+	function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+
+button.addEventListener("click", setBg);
+
+button.addEventListener("click", setBg1);
